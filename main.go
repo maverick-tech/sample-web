3cfb5dd250e3786ed42bb474361f46c32929f461
maverick-tech <tarlemanmohan@outlook.com>
2020-06-25 21:22:14 +0530

    add port

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-)
+	"os"
 
 func webHandlerFunc(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "<h1>Hello, World</h1>")
 }
 
 func main() {
+	port:=os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/", webHandlerFunc)
-	http.ListenAndServe(":3030", nil)
+	http.ListenAndServe(":" + port, nil)
 }
